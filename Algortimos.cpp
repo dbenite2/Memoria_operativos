d1d9a166b284f8a6a1eda6c747815b1c56ff3d2b
David <flio034@gmail.com>
2018-06-05 15:12:16 -0500

    cambios mayores al codigo en general, se modifica main, fifo.

--- Algortimos.cpp
+++ Algortimos.cpp
@@ -11,98 +11,108 @@ using namespace std;
 
 int sacarBase(string dato,int bitAddress,int tamPag){
   
-  //string s = "0xFF";
   double desplazamiento = log2(tamPag);
-  int numPag = bitAddress - desplazamiento;
+  int numPag = 0;
+  cout << "desplazamiento: " << desplazamiento << endl;
+    numPag = bitAddress - desplazamiento;
+    
+
+  cout << "numPag: " << numPag << endl;
   stringstream ss;
   ss << hex << dato;
   unsigned n;
   ss >> n;
   bitset<8> b(n);
+//   if(bitAddress%2 == 0){
+//     if(bitAddress == 2){
+//         bitset<2> b(n);
+//     }else if(bitAddress == 4){
+//         bitset<4> b(n);
+//     }else if(bitAddress == 8){
+//         bitset<8> b(n);
+//     }else if(bitAddress == 16){
+//         bitset<16> b(n);
+//     }else if(bitAddress == 32){
+//         bitset<32> b(n);
+//     }else if(bitAddress == 64){
+//         bitset<64> b(n);
+//     }else{
+//         bitset<128> b(n);
+//     }
+  
   string direccion = b.to_string();
+  cout << "base pagina: " << direccion << endl;
   int address = stoi(direccion.substr(0,numPag),0,2);
   return address;
 }
 
+int fifo(int bitAddress,int tamPag, int *marcos,int frames,int siguiente,int cont){
+    string dato;
+    while (cin >> dato){
+        int base = sacarBase(dato,bitAddress,tamPag);
+        cout << "base: " << base << endl;
+        int i = 0;
+        bool salir = false;
+        if(cont == frames){
+            while(i<frames && !salir){
+                if (marcos[i] == base){
+                    salir = true; 
+                }
+                i++;
+            }
+        
+            //FIFO
+            if(!salir){
+                marcos[siguiente] = base;
+                siguiente = (siguiente+1) % frames;
+            
+            }
+        }else{
+            
+            while(i < cont && !salir){
+                if(marcos[i] == base){
+                    salir = true;
+                }
+                i++;
+            }
+                
+            if(!salir){
+                marcos[cont] = base;
+                cont++;
+            }
+        }
+        cout << "direccion: " << dato;
+        for(int i = 0; i< frames; i++){
+            cout << "  f" << i << ": " << marcos[i];
+        }
+        cout << endl;    
+        
+        
+    }
+}
+
 int main(){
-    int bitAddress = 0; //8
+    int bitAddress; //8
     int tamPag = 0; //32
-    size_t frames = 0; //3
+    int frames = 0; //3
     int algoritmo = 0; //0
     cout << "ingrese parametros" << endl;
     cin >> bitAddress >> tamPag >> frames >> algoritmo;
     int marcos [frames]; //frames
-    unsigned int marcos_Length(sizeof(marcos)/sizeof(marcos[0]));
-    for (int i  = 0;i<marcos_Length;i++){
-        marcos[i] = -1;
-        cout << marcos[i] << endl;
-    }
-    int siguiente = -1;
+    // unsigned int marcos_Length(sizeof(marcos)/sizeof(marcos[0]));
+    // for (int i  = 0;i<marcos_Length;i++){
+    //     marcos[i] = -1;
+    //     cout << marcos[i] << endl;
+    // }
+    int siguiente = 0;
     int cont = 0;
     int leerDireccion = 0;
-    string dato;
-
-    while (cin >> dato){
-        for(int i = 0; i< bitAddress; i++){
-            int base = sacarBase(dato,bitAddress,tamPag);
-            
-            int flag = 0;
-            for(int j = 0; j < marcos_Length;j++){
-                if (base == marcos[j]){
-                flag = 1;
-                
-                break;
-                }
-            }
-            if(flag == 0){
-                siguiente = (siguiente + 1 ) % frames;
-                marcos[siguiente] = base;
-                cout << "dato: " << dato << " " << "f" << marcos[0] << " f2: " << marcos[1] << " f3: " << marcos[2] << endl;
-            }else{
-                cout << "dato: " << dato << " " << "f" << marcos[0] << " f2: " << marcos[1] << " f3: " << marcos[2] << endl;
-            }
-        }
-    //     int base = sacarBase(dato,bitAddress,tamPag);
-    //     int i = 0;
-    //     bool salir;
-    //     cout << "entrando" << endl;
-    //     if(cont == frames -1){
-    //         cout << "entro 1" << endl;
-    //         salir = false;
-    //         while(i<frames && !salir){
-    //             if (marcos[i] == base){
-    //                 salir = true; 
-    //             }
-    //             i++;
-    //         }
-        
-    //         //FIFO
-    //         if(!salir){
-    //             marcos[siguiente] = base;
-    //             cout << "siguiente " << siguiente << " " << "dato: " << dato << " " << "f1 " << marcos[0] << " f2 " << marcos[1] << " f3 " << marcos[2] << endl;   
-    //             siguiente++;
-    //             siguiente = siguiente % frames;
-            
-    //         }
-    //     }else{
-            
-    //         while(i < frames){
-    //             cout << "entro 2" << endl;
-    //             if(marcos[i] == base){
-    //                 salir = true;
-    //             }
-    //             i++;
-    //             if(!salir){
-    //                 marcos[cont] = base;
-    //             }
-    //             cont++;
-        
-    //         }    
-    //     }
-        
-     }
-        return 0;
+    if(algoritmo == 0){
+    fifo(bitAddress,tamPag,marcos,frames,siguiente,cont);
+    }
+    return 0;
 }
+
 
 
        
